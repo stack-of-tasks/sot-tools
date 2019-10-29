98d7d40b514d884d76f99e14ab5c5521f66610e0
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-10-29 21:48:13 +0100

    Format

--- src/kinematic-planner.hh
+++ src/kinematic-planner.hh
@@ -7,7 +7,6 @@
 #ifndef SOT_TOOLS_KINEMATIC_PLANNER_HH
 #define SOT_TOOLS_KINEMATIC_PLANNER_HH
 
-
 /* STD */
 #include <string>
 #include <sstream>
@@ -33,96 +32,86 @@
 /* BOOST */
 //#include <boost/filesystem.hpp>
 
-namespace dynamicgraph {  namespace sot {  namespace tools {
-      
-      using dynamicgraph::Entity;
-      
-      class KinematicPlanner : public Entity
-      {
-      public:
-	DYNAMIC_GRAPH_ENTITY_DECL();
-	typedef std::vector<Eigen::ArrayXd,Eigen::aligned_allocator<Eigen::ArrayXd> >
-	stdVectorofArrayXd;
+namespace dynamicgraph {
+namespace sot {
+namespace tools {
 
-	typedef std::vector<Eigen::ArrayXXd,Eigen::aligned_allocator<Eigen::ArrayXXd> >
-	stdVectorofArrayXXd;
+using dynamicgraph::Entity;
 
-	/*-----SIGNALS--------*/
-	typedef int Dummy;
-	/*
-	  dynamicgraph::SignalPtr<double,int> distToDrawerSIN;
-	  dynamicgraph::SignalPtr<double,int> objectPositionInDrawerSIN;
-	  
-	  dynamicgraph::SignalTimeDependent<Dummy,int> trajectoryReadySINTERN;
-	  
-	  dynamicgraph::SignalTimeDependent<dynamicgraph::Matrix, int> upperBodyJointPositionSOUT;
-	  dynamicgraph::SignalTimeDependent<dynamicgraph::Matrix, int> upperBodyJointVelocitySOUT;
-	  dynamicgraph::SignalTimeDependent<dynamicgraph::Matrix, int> freeFlyerVelocitySOUT;
-	*/
-	/* --- CONSTRUCTOR --- */
-	KinematicPlanner( const std::string& name);
-	virtual ~KinematicPlanner( void );
-	//Sources
-	Eigen::ArrayXd npSource;
-	Eigen::ArrayXXd pSource1;
-	Eigen::ArrayXXd pSource2;
+class KinematicPlanner : public Entity {
+ public:
+  DYNAMIC_GRAPH_ENTITY_DECL();
+  typedef std::vector<Eigen::ArrayXd, Eigen::aligned_allocator<Eigen::ArrayXd> > stdVectorofArrayXd;
 
-	stdVectorofArrayXXd pSourceDelayed1;
-	stdVectorofArrayXXd pSourceDelayed2;
-	//Delays
-	Eigen::ArrayXXd pDelay1;
-	Eigen::ArrayXXd pDelay2;
-	
-	//Non Periodic Weights
-	Eigen::ArrayXXd wNonPeriodic; //Eigen::Array<double, 480,4>
-	
-	//Periodic Weights
-	stdVectorofArrayXXd wPeriodic1;
-	stdVectorofArrayXXd wPeriodic2;
+  typedef std::vector<Eigen::ArrayXXd, Eigen::aligned_allocator<Eigen::ArrayXXd> > stdVectorofArrayXXd;
 
-	//Mean joint angles
-	Eigen::ArrayXXd mJointAngle;
+  /*-----SIGNALS--------*/
+  typedef int Dummy;
+  /*
+    dynamicgraph::SignalPtr<double,int> distToDrawerSIN;
+    dynamicgraph::SignalPtr<double,int> objectPositionInDrawerSIN;
 
-	//Number of Trajectories Created
-	//	int nTrajectories; //30
-	int nJoints; //16
-	int nGaitCycles; //4
-	int nTimeSteps; //160
-	int nSources1; //5
-	int nSources2; //4
-	/*! @} */
-	std::list< dynamicgraph::SignalBase<int>*  > genericSignalRefs;
+    dynamicgraph::SignalTimeDependent<Dummy,int> trajectoryReadySINTERN;
 
-	//Load Motion Capture outputs
-	template<typename Derived>
-	void read2DArray(std::string& fileName,
-			 Eigen::DenseBase<Derived>& outArr);
+    dynamicgraph::SignalTimeDependent<dynamicgraph::Matrix, int> upperBodyJointPositionSOUT;
+    dynamicgraph::SignalTimeDependent<dynamicgraph::Matrix, int> upperBodyJointVelocitySOUT;
+    dynamicgraph::SignalTimeDependent<dynamicgraph::Matrix, int> freeFlyerVelocitySOUT;
+  */
+  /* --- CONSTRUCTOR --- */
+  KinematicPlanner(const std::string& name);
+  virtual ~KinematicPlanner(void);
+  // Sources
+  Eigen::ArrayXd npSource;
+  Eigen::ArrayXXd pSource1;
+  Eigen::ArrayXXd pSource2;
 
-	void setParams(	const double& _distanceToDrawer,
-			const double& _objectPositionInDrawer,
-			const std::string& dir);
-	void loadSourceDelays(const std::string& dir);
-	void loadTrainingParams(const std::string &dir
-				,dynamicgraph::Matrix &q
-				,dynamicgraph::Matrix &beta3
-				,Eigen::ArrayXd &mwwn
-				,double &sigma2
-				,int &N
-				,int &K);
-	dynamicgraph::Vector createSubGoals(double D, double P);
-	void delaySources();
-	void blending();
-	void smoothEnds(Eigen::Ref<Eigen::ArrayXd> tr);
-	void bSplineInterpolate(Eigen::ArrayXXd& tr, int factor);
-	int& runKinematicPlanner(int& dummy, int time);
-	void goalAdaption(dynamicgraph::Vector& goals, const std::string&);
-	void savitzkyGolayFilter(Eigen::Ref<Eigen::ArrayXXd> allJointTraj, int polyOrder, int frameSize);
+  stdVectorofArrayXXd pSourceDelayed1;
+  stdVectorofArrayXXd pSourceDelayed2;
+  // Delays
+  Eigen::ArrayXXd pDelay1;
+  Eigen::ArrayXXd pDelay2;
 
+  // Non Periodic Weights
+  Eigen::ArrayXXd wNonPeriodic;  // Eigen::Array<double, 480,4>
 
-	bool parametersSet;
-      }; // class KinematicPlanner
-    } // namespace tools
-  } //namespace sot
-} // namespace dynamicgraph
+  // Periodic Weights
+  stdVectorofArrayXXd wPeriodic1;
+  stdVectorofArrayXXd wPeriodic2;
 
-#endif //SOT_TOOLS_KINEMATIC_PLANNER_HH
+  // Mean joint angles
+  Eigen::ArrayXXd mJointAngle;
+
+  // Number of Trajectories Created
+  //	int nTrajectories; //30
+  int nJoints;      // 16
+  int nGaitCycles;  // 4
+  int nTimeSteps;   // 160
+  int nSources1;    // 5
+  int nSources2;    // 4
+  /*! @} */
+  std::list<dynamicgraph::SignalBase<int>*> genericSignalRefs;
+
+  // Load Motion Capture outputs
+  template <typename Derived>
+  void read2DArray(std::string& fileName, Eigen::DenseBase<Derived>& outArr);
+
+  void setParams(const double& _distanceToDrawer, const double& _objectPositionInDrawer, const std::string& dir);
+  void loadSourceDelays(const std::string& dir);
+  void loadTrainingParams(const std::string& dir, dynamicgraph::Matrix& q, dynamicgraph::Matrix& beta3,
+                          Eigen::ArrayXd& mwwn, double& sigma2, int& N, int& K);
+  dynamicgraph::Vector createSubGoals(double D, double P);
+  void delaySources();
+  void blending();
+  void smoothEnds(Eigen::Ref<Eigen::ArrayXd> tr);
+  void bSplineInterpolate(Eigen::ArrayXXd& tr, int factor);
+  int& runKinematicPlanner(int& dummy, int time);
+  void goalAdaption(dynamicgraph::Vector& goals, const std::string&);
+  void savitzkyGolayFilter(Eigen::Ref<Eigen::ArrayXXd> allJointTraj, int polyOrder, int frameSize);
+
+  bool parametersSet;
+};  // class KinematicPlanner
+}  // namespace tools
+}  // namespace sot
+}  // namespace dynamicgraph
+
+#endif  // SOT_TOOLS_KINEMATIC_PLANNER_HH
--- src/simpleseqplay.hh
+++ src/simpleseqplay.hh
@@ -1,65 +1,64 @@
 //
 // Copyright (C) 2012, 2013 LAAS-CNRS
 //
-// From Author: Florent Lamiraux, Mehdi Benallegue, 
+// From Author: Florent Lamiraux, Mehdi Benallegue,
 // Author: Olivier Stasse
 // Simple sequence player just playing back a set of poses.
 //
 
 #ifndef SOT_TOOLS_SIMPLE_SEQPLAY_HH
-# define SOT_TOOLS_SIMPLE_SEQPLAY_HH
+#define SOT_TOOLS_SIMPLE_SEQPLAY_HH
 
-# include <iostream>
-# include <sstream>
-# include <fstream>
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
-# include <dynamic-graph/entity.h>
-# include <dynamic-graph/factory.h>
-# include <dynamic-graph/linear-algebra.h>
+#include <dynamic-graph/entity.h>
+#include <dynamic-graph/factory.h>
+#include <dynamic-graph/linear-algebra.h>
 #include <dynamic-graph/signal-ptr.h>
-# include <dynamic-graph/signal-time-dependent.h>
-# include <sot/core/matrix-geometry.hh>
+#include <dynamic-graph/signal-time-dependent.h>
+#include <sot/core/matrix-geometry.hh>
 
 namespace dynamicgraph {
-  namespace sot {
-    namespace tools {
-      namespace dg = dynamicgraph;
+namespace sot {
+namespace tools {
+namespace dg = dynamicgraph;
 
-      class SimpleSeqPlay : public dg::Entity
-      {
-	typedef int Dummy;
-	dg::SignalTimeDependent<Dummy,int> firstSINTERN;
-	dg::SignalTimeDependent<dg::Vector,int> postureSOUT_;
-	
-	dg::SignalPtr <dg::Vector,int> currentPostureSIN_;
-	
-	DYNAMIC_GRAPH_ENTITY_DECL();
-	SimpleSeqPlay (const std::string& name);
+class SimpleSeqPlay : public dg::Entity {
+  typedef int Dummy;
+  dg::SignalTimeDependent<Dummy, int> firstSINTERN;
+  dg::SignalTimeDependent<dg::Vector, int> postureSOUT_;
 
-	void load (const std::string& filename);
-	void start ();
-	virtual std::string getDocString () const;
+  dg::SignalPtr<dg::Vector, int> currentPostureSIN_;
 
-      private:
-	dg::Vector& computePosture (dg::Vector& pos, int t);
-	// 0: motion not started, 1: going to the current position to the first position.
-	// 2: motion in progress, 3: motion finished
-	unsigned int state_;
-	unsigned int configId_;
-	int startTime_;
+  DYNAMIC_GRAPH_ENTITY_DECL();
+  SimpleSeqPlay(const std::string& name);
 
-	std::vector <dg::Vector> posture_;
-	dg::Vector currentPosture_;
+  void load(const std::string& filename);
+  void start();
+  virtual std::string getDocString() const;
 
-	std::vector <double> time_;
-	double dt_;
-	double time_to_start_;
-	// Number of iterations performed in state1.
-	int it_nbs_in_state1_;
+ private:
+  dg::Vector& computePosture(dg::Vector& pos, int t);
+  // 0: motion not started, 1: going to the current position to the first position.
+  // 2: motion in progress, 3: motion finished
+  unsigned int state_;
+  unsigned int configId_;
+  int startTime_;
 
-      }; // class SimpleSeqPlay
-    } // namespace tools
-  } //namespace sot
-} // namespace dynamicgraph
+  std::vector<dg::Vector> posture_;
+  dg::Vector currentPosture_;
 
-#endif //SOT_TOOLS_SIMPLESEQPLAY_HH
+  std::vector<double> time_;
+  double dt_;
+  double time_to_start_;
+  // Number of iterations performed in state1.
+  int it_nbs_in_state1_;
+
+};  // class SimpleSeqPlay
+}  // namespace tools
+}  // namespace sot
+}  // namespace dynamicgraph
+
+#endif  // SOT_TOOLS_SIMPLESEQPLAY_HH

652a41fa1b090d7ec7310534310438896e8d633f
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-03-10 08:59:40 +0100

    [CMake] clean: keep minimal required instructions

--- tests/test_simpleseqplay.cc
+++ tests/test_simpleseqplay.cc
@@ -5,26 +5,21 @@
 #include "simpleseqplay.hh"
 
 namespace dg = dynamicgraph;
-BOOST_AUTO_TEST_SUITE ( BOOST_TEST_MODULE )
+BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
-BOOST_AUTO_TEST_CASE(test_simpleseqplay)
-{
+BOOST_AUTO_TEST_CASE(test_simpleseqplay) {
   dg::sot::tools::SimpleSeqPlay aSimpleSeqPlay("simpleseqplay");
 
   aSimpleSeqPlay.load("test");
   aSimpleSeqPlay.start();
-  for(int i=0;i<1000;i++)
-    {
-      dg::Vector pos;
-      
-      pos = aSimpleSeqPlay.postureSOUT_(i);
-      if (i==100)
-	aSimpleSeqPlay.hold();
+  for (int i = 0; i < 1000; i++) {
+    dg::Vector pos;
 
-      if (i==300)
-	aSimpleSeqPlay.unhold();
-    }
- 
+    pos = aSimpleSeqPlay.postureSOUT_(i);
+    if (i == 100) aSimpleSeqPlay.hold();
+
+    if (i == 300) aSimpleSeqPlay.unhold();
+  }
 }
 
-BOOST_AUTO_TEST_SUITE_END ()
+BOOST_AUTO_TEST_SUITE_END()

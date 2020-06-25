1f2a7814908d40bf3c88442670ea15513ed16bff
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-06-25 19:16:51 +0200

    [Python][Tests] ensure oscillator loads fine
    fix #17

---
+++ tests/python/oscillator.py
@@ -0,0 +1,16 @@
+import unittest
+from random import random
+
+from dynamic_graph.sot.tools import Oscillator
+
+
+class OscillatorTest(unittest.TestCase):
+    def test_load(self):
+        epsilon = random()
+        osc = Oscillator('my oscillator')
+        osc.setEpsilon(epsilon)
+        self.assertEqual(osc.getEpsilon(), epsilon)
+
+
+if __name__ == '__main__':
+    unittest.main()

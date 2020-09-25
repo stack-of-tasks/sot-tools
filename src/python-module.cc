5686b5a0168cd12c2d2ac3ea30cf8b2418e88969
Joseph Mirabel <jmirabel@laas.fr>
2020-09-25 22:01:40 +0200

    Add function to get state of SimpleSeqPlay

---
+++ src/python-module.cc
@@ -0,0 +1,40 @@
+#include "dynamic-graph/python/module.hh"
+
+#include <sot/tools/cubic-interpolation.hh>
+#include <sot/tools/cubic-interpolation-se3.hh>
+#include <sot/tools/kinematic-planner.hh>
+#include <sot/tools/oscillator.hh>
+#include <sot/tools/seqplay.hh>
+#include <sot/tools/simpleseqplay.hh>
+
+namespace dgst = dynamicgraph::sot::tools;
+
+typedef boost::mpl::vector<
+  dgst::CubicInterpolation
+, dgst::CubicInterpolationSE3
+, dgst::Oscillator
+, dgst::Seqplay
+> entities_t;
+
+struct register_entity
+{
+template<typename T> inline void operator()(boost::type<T>) const
+{
+  dynamicgraph::python::exposeEntity<T>();
+}
+};
+
+BOOST_PYTHON_MODULE(wrap)
+{
+  bp::import("dynamic_graph");
+  boost::mpl::for_each<entities_t, boost::type<boost::mpl::_> >(register_entity());
+
+  using dgst::SimpleSeqPlay;
+  dynamicgraph::python::exposeEntity<SimpleSeqPlay>()
+    .def("waiting", &SimpleSeqPlay::waiting)
+    .def("initializing", &SimpleSeqPlay::initializing)
+    .def("executing", &SimpleSeqPlay::executing)
+    .def("finished", &SimpleSeqPlay::finished)
+  ;
+}
+

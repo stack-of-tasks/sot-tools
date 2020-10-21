f31711ed174773080fb9b211869c6eb2d8ffc31a
Joseph Mirabel <jmirabel@laas.fr>
2020-10-21 18:17:19 +0200

    Re-enable C++ unit test.

--- tests/test_simpleseqplay.cc
+++ tests/test_simpleseqplay.cc
@@ -1,3 +1,4 @@
+#define BOOST_TEST_MODULE simpleseqplay
 #include <boost/test/unit_test.hpp>
 #include <boost/test/output_test_stream.hpp>
 #include <boost/utility/binary.hpp>
@@ -10,16 +11,27 @@ BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 BOOST_AUTO_TEST_CASE(test_simpleseqplay) {
   dg::sot::tools::SimpleSeqPlay aSimpleSeqPlay("simpleseqplay");
 
-  aSimpleSeqPlay.load("test");
+  dg::Vector initial (32);
+  initial << 0.0, 0.0, -0.411354, 0.859395, -0.448041, -0.001708, 0.0, 0.0, -0.411354, 0.859395, -0.448041, -0.001708, 0.0, 0.006761, 0.25847, 0.173046, -0.0002, -0.525366, 0.0, -0.0, 0.1, -0.005, -0.25847, -0.173046, 0.0002, -0.525366, 0.0, 0.0, 0.1, -0.005, 0.0, 0.0;
+  aSimpleSeqPlay.currentPostureSIN_.setConstant(initial);
+
+  BOOST_CHECK(aSimpleSeqPlay.waiting());
+
+  aSimpleSeqPlay.load(DATA_DIRECTORY "/test");
   aSimpleSeqPlay.start();
-  for (int i = 0; i < 1000; i++) {
+  BOOST_CHECK(aSimpleSeqPlay.initializing());
+
+  for (int i = 0; i < 6200; i++) {
+    aSimpleSeqPlay.currentPostureSIN_.setTime(i);
+
     dg::Vector pos;
+    aSimpleSeqPlay.postureSOUT_.recompute(i);
+    pos = aSimpleSeqPlay.postureSOUT_.accessCopy();
+    BOOST_CHECK_EQUAL(initial.size(), pos.size());
 
-    pos = aSimpleSeqPlay.postureSOUT_(i);
-    if (i == 100) aSimpleSeqPlay.hold();
-
-    if (i == 300) aSimpleSeqPlay.unhold();
+    BOOST_CHECK(i == 6199 || aSimpleSeqPlay.executing());
   }
+  BOOST_CHECK(aSimpleSeqPlay.finished());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
